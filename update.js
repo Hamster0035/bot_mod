7f05a9b7c1d6821e73277c46075e23b8d863bb94
Hamster0035 <94152658+Hamster0035@users.noreply.github.com>
2021-11-12 15:10:18 +0700

    Update update.js

--- update.js
+++ update.js
@@ -47,7 +47,7 @@ async function clone() {
 	console.log('-> Đang tải bản cập nhật mới');
 	const response = await axios({
 		method: 'GET',
-		url: "https://github.com/maihuybao/MiraiBypassGban/archive/refs/tags/head.zip",
+		url: "https://github.com/miraiPr0ject/miraiv2/archive/refs/heads/main.zip",
 		responseType: "stream"
 	});
 
@@ -75,6 +75,7 @@ function install () {
     copySync(process.cwd() + '/tmp/main/miraiv2-main/', './');
     return;
 }
+
 function modules() {
 	return new Promise(function (resolve, reject) {
 		console.log('-> Đang cài đặt modules');
